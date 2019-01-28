0f62de72123e953f375c390340a96001bb89b58c
Paul Rosenberg <paulrosenberg13@gmail.com>
2019-01-27 19:37:00 -0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ToDoItem from './ToDoItem';
+
+
+function App() {
+  return (
+    <div>
+      <h1>ToDo App</h1>
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ToDoItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const styleToDo = {
+  
+}
+
+function ToDoItem() {
+  return (
+    <div>
+    <input type="checkbox" />
+    <p>Placeholder</p>
+  </div>
+  );
+}
+
+const ToDo = () =>  (
+  <div>
+    <input type="checkbox" />
+    <p>Placeholder</p>
+  </div>);
+
+  export default ToDoItem;
